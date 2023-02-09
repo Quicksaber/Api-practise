6759c14ac32149bc60087e5942b2ef6d3f59f17e
Quicksaber <94722569+Quicksaber@users.noreply.github.com>
2023-02-09 09:46:36 +0100

    stubbing try one

---
+++ cypress/e2e/smoke/stubbingResponse.spec.cy.js
@@ -0,0 +1,24 @@
+import organization from "../../API/organization";
+import board from "../../API/boards";
+import apiLogin from "../../API/login";
+import { randomStringGenerator } from "../../support/generator";
+import data from "../../fixtures/data.json";
+import myOrgs from "../../fixtures/myOrgs.json";
+
+describe("api stubbing", () => {
+  beforeEach(() => {
+    cy.sessionLogin(Cypress.env("email"), Cypress.env("password"));
+  });
+
+  it("stubbing test for organizations", () => {
+    cy.intercept({
+      method: "GET",
+      url: `${Cypress.env("apiCypressVivify")}my-organizations`},
+      
+      {headers: {
+        Authorization: `Bearer ${window.localStorage.getItem("token")}`,
+      },
+      fixture: {'myOrgs.json'}
+    });
+  });
+});
